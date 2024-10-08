eea56b98ba4b11507d1fe310e68733de15a47b31
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2024-10-09 00:35:55 +0300

    add daily backup script

---
+++ db-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DB="./db/users.sqlite"  
+BACKUP_DIR="./db-backup"  
+DATE=$(date +"%Y-%m-%d")
+
+BACKUP_FILE="${BACKUP_DIR}/sqlite_backup_${DATE}.sqlite"
+
+if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir -p "$BACKUP_DIR"
+fi
+
+cp "$SOURCE_DB" "$BACKUP_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "Backup successful: $BACKUP_FILE"
+else
+    echo "Backup failed!"
+    exit 1
+fi
