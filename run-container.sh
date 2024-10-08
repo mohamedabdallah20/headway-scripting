25ea0820238a1df3554417645d43649f27a34ab3
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2024-10-09 00:08:40 +0300

    pulling before run container

--- run-container.sh
+++ run-container.sh
@@ -11,6 +11,9 @@ container_name=node-sqlite-app-container
 
 ./stop-remove-container.sh
 
+echo "Pulling latest Docker image..."
+docker pull $account/$image_name:$tag >> $log_file 2>&1
+
 echo "Running Docker container..."
 docker run -d -p 3000:3000  -v "$(pwd)"/db/:/app/db/ --name=$container_name $account/$image_name:$tag >> $log_file 2>&1
 
